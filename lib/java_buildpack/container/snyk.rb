000a65021583d3867ff6a47e5e799dcc66230875
Arthur Granado <agranado2k@gmail.com>
2019-06-25 22:12:25 +0100

    Test 1

---
+++ lib/java_buildpack/container/snyk.rb
@@ -0,0 +1,25 @@
+require 'java_buildpack/component/base_component'
+require 'java_buildpack/container'
+require 'java_buildpack/util/dash_case'
+require 'java_buildpack/util/java_main_utils'
+require 'java_buildpack/util/qualify_path'
+require 'java_buildpack/util/spring_boot_utils'
+
+module JavaBuildpack
+  module Container
+    # Encapsulates the detect, compile, and release functionality for applications running a Snyk integration
+    class Snyk < JavaBuildpack::Component::BaseComponent
+      def initialize(context)
+        puts "context:"
+        puts context
+        super(context)
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#detect)
+      def detect
+        puts "My Snyk detect"
+        nil
+      end
+    end
+  end
+end
