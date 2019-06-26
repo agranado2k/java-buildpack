b1e4707b0cd414e964a1f8d5c6e567b823849b0d
Arthur Granado <agranado2k@gmail.com>
2019-06-26 22:30:45 +0100

    Test 14

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@
 
 source 'https://rubygems.org'
 
-gem 'rest-client', '~> 2.0', '>= 2.0.2'
-
 group :development do
   gem 'rake'
   gem 'redcarpet'
--- lib/java_buildpack/framework/snyk.rb
+++ lib/java_buildpack/framework/snyk.rb
@@ -5,7 +5,8 @@ require 'java_buildpack/util/java_main_utils'
 require 'java_buildpack/util/qualify_path'
 require 'java_buildpack/util/spring_boot_utils'
 require 'digest'
-require 'rest-client'
+require "net/http"
+require "uri"
 
 module JavaBuildpack
   module Framework
@@ -15,7 +16,6 @@ module JavaBuildpack
         super(context, &version_validator)
         @mvn_org_base_api = context[:configuration]['mvn_org_base_api']
         @vunl_service_base_api = context[:configuration]['vunl_service_base_api']
-        @client = RestClient
       end
 
       # (see JavaBuildpack::Component::BaseComponent#detect)
@@ -30,8 +30,13 @@ module JavaBuildpack
           puts file_path
           sha1 = Digest::SHA1.file file_path
           sha1_hash = sha1.hexdigest
-          puts "Checksum SHA1: #{sha1_hash}"
-          response = @client.get "#{mvn_org_base_api}/solrsearch/select?q=1:'#{sha1_hash}'&wt=json"
+          puts "Checksum SHA1: #{ sha1_hash}"
+
+          url = "#{mvn_org_base_api}/solrsearch/select?q=1:'#{sha1_hash}'&wt=json"
+          uri = URI.parse(url)
+          http = Net::HTTP.new(uri.host, uri.port)
+          request = Net::HTTP::Get.new("/search?question=somequestion")
+          response = http.request(request)
           json = JSON.parse(response.body, symbolize_names: true)
           puts "Response:"
           puts json
