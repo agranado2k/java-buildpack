b8790e8a485b96c2f0b6f9d939afee79332d3351
Arthur Granado <agranado2k@gmail.com>
2019-06-26 22:42:36 +0100

    Test 17

--- lib/java_buildpack/framework/snyk.rb
+++ lib/java_buildpack/framework/snyk.rb
@@ -12,6 +12,8 @@ module JavaBuildpack
   module Framework
     # Encapsulates the detect, compile, and release functionality for applications running a Snyk integration
     class Snyk < JavaBuildpack::Component::BaseComponent
+      LIB_DIR = "/tmp/app/WEB-INF/lib"
+
       def initialize(context, &version_validator)
         super(context, &version_validator)
         @mvn_org_base_api = context[:configuration]['mvn_org_base_api']
@@ -23,7 +25,7 @@ module JavaBuildpack
         puts "My Snyk detect"
         puts "mvn_org_base_api: #{@mvn_org_base_api}"
         puts "vunl_service_base_api: #{@vunl_service_base_api}"
-        Dir.chdir("/tmp/app/WEB-INF/lib")
+        Dir.chdir(LIB_DIR)
         dir =  Dir.pwd
         Dir.glob("*").each do |file|
           file_path = "#{dir}/#{file}"
@@ -32,27 +34,27 @@ module JavaBuildpack
         end
         "Snyk"
       end
-    end
 
-    private
+      private
 
-    def sha1_for(file_path)
-      puts file_path
-      sha1 = Digest::SHA1.file file_path
-      sha1_hash = sha1.hexdigest
-      puts "Checksum SHA1: #{sha1_hash}"
-      sha1_hash
-    end
+      def sha1_for(file_path)
+        puts file_path
+        sha1 = Digest::SHA1.file file_path
+        sha1_hash = sha1.hexdigest
+        puts "Checksum SHA1: #{sha1_hash}"
+        sha1_hash
+      end
 
-    def mvn_dependency_info(sha1_hash)
-      uri = URI.parse(@mvn_org_base_api)
-      http = Net::HTTP.new(uri.host, uri.port)
-      request = Net::HTTP::Get.new("/solrsearch/select?q=1:'#{sha1_hash}'&wt=json")
-      response = http.request(request)
-      json = JSON.parse(response.body, symbolize_names: true)
-      puts "Response:"
-      puts json
-      json
+      def mvn_dependency_info(sha1_hash)
+        uri = URI.parse(@mvn_org_base_api)
+        http = Net::HTTP.new(uri.host, uri.port)
+        request = Net::HTTP::Get.new("/solrsearch/select?q=1:'#{sha1_hash}'&wt=json")
+        response = http.request(request)
+        json = JSON.parse(response.body, symbolize_names: true)
+        puts "Response:"
+        puts json
+        json
+      end
     end
   end
 end
