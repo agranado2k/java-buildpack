46dfc911da128a5940ef251857815b21087ac843
Arthur Granado <agranado2k@gmail.com>
2019-06-26 22:37:51 +0100

    Test 16

--- lib/java_buildpack/framework/snyk.rb
+++ lib/java_buildpack/framework/snyk.rb
@@ -27,22 +27,32 @@ module JavaBuildpack
         dir =  Dir.pwd
         Dir.glob("*").each do |file|
           file_path = "#{dir}/#{file}"
-          puts file_path
-          sha1 = Digest::SHA1.file file_path
-          sha1_hash = sha1.hexdigest
-          puts "Checksum SHA1: #{ sha1_hash}"
-
-          url = "#{@mvn_org_base_api}/solrsearch/select?q=1:'#{sha1_hash}'&wt=json"
-          uri = URI.parse(url)
-          http = Net::HTTP.new(uri.host, uri.port)
-          request = Net::HTTP::Get.new("/search?question=somequestion")
-          response = http.request(request)
-          json = JSON.parse(response.body, symbolize_names: true)
-          puts "Response:"
-          puts json
+          sha1_hash = sha1_for(file_path)
+          mvn_dependency_info(sha1_hash)
         end
         "Snyk"
       end
+    end
+
+    private
+
+    def sha1_for(file_path)
+      puts file_path
+      sha1 = Digest::SHA1.file file_path
+      sha1_hash = sha1.hexdigest
+      puts "Checksum SHA1: #{sha1_hash}"
+      sha1_hash
+    end
+
+    def mvn_dependency_info(sha1_hash)
+      uri = URI.parse(@mvn_org_base_api)
+      http = Net::HTTP.new(uri.host, uri.port)
+      request = Net::HTTP::Get.new("/solrsearch/select?q=1:'#{sha1_hash}'&wt=json")
+      response = http.request(request)
+      json = JSON.parse(response.body, symbolize_names: true)
+      puts "Response:"
+      puts json
+      json
     end
   end
 end
